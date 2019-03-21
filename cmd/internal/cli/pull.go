0e8ee02047a83bf3a4c1284797cb07ba1f2f7800
Ian <iankane@umich.edu>
2019-03-21 14:54:37 -0400

    check supported OCI transports instead of only looking for docker

--- cmd/internal/cli/pull.go
+++ cmd/internal/cli/pull.go
@@ -14,6 +14,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
 	"github.com/sylabs/singularity/internal/pkg/client/cache"
+	ociclient "github.com/sylabs/singularity/internal/pkg/client/oci"
 	"github.com/sylabs/singularity/internal/pkg/libexec"
 	"github.com/sylabs/singularity/internal/pkg/sylog"
 	"github.com/sylabs/singularity/internal/pkg/util/uri"
@@ -28,8 +29,6 @@ const (
 	// ShubProtocol holds singularity hub base URI
 	// for more info refer to https://singularity-hub.org/
 	ShubProtocol = "shub"
-	// DockerProtocol holds dockerhub base URI
-	DockerProtocol = "docker"
 	// HTTPProtocol holds the remote http base URI
 	HTTPProtocol = "http"
 	// HTTPSProtocol holds the remote https base URI
@@ -170,7 +169,7 @@ func pullRun(cmd *cobra.Command, args []string) {
 		libexec.PullShubImage(name, args[i], force, noHTTPS)
 	case HTTPProtocol, HTTPSProtocol:
 		libexec.PullNetImage(name, args[i], force)
-	case DockerProtocol:
+	case ociclient.IsSupported(transport):
 		if !force {
 			if _, err := os.Stat(name); err == nil {
 				sylog.Fatalf("image file already exists - will not overwrite")

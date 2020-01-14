c2538974d39f9efabb864ef91a4ab55a95fda40f
David Trudgian <david.trudgian@sylabs.io>
2020-01-14 11:25:32 -0600

    Catch missing version prefix change

--- e2e/version/version.go
+++ e2e/version/version.go
@@ -63,6 +63,7 @@ func (c ctx) testEqualVersion(t *testing.T) {
 
 		checkEqualVersionFn := func(t *testing.T, r *e2e.SingularityCmdResult) {
 			outputVer := strings.TrimPrefix(string(r.Stdout), "singularity version ")
+			outputVer = strings.TrimPrefix(outputVer, "SingularityPRO version ")
 			outputVer = strings.TrimSpace(outputVer)
 			semanticVersion, err := semver.Make(outputVer)
 			if err != nil {
